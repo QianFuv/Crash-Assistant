2ad79ddeb01d6ebcd1e1baad3600e6a5e8999215
KostromDan <kostromdan@gmail.com>
2024-11-27 22:17:15 +0300

    mixin

---
+++ src/main/java/dev/kostromdan/mods/crash_assistant/mixins/MixinCrashReport.java
@@ -0,0 +1,21 @@
+package dev.kostromdan.mods.crash_assistant.mixins;
+
+import dev.kostromdan.mods.crash_assistant.CrashAssistant;
+import net.minecraft.CrashReport;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.File;
+
+
+@Mixin(CrashReport.class)
+public class MixinCrashReport {
+    @Inject(method = "saveToFile", at = @At("RETURN"))
+    private void afterSaveToFile(File p_127513_, CallbackInfoReturnable<Boolean> cir) {
+        CrashAssistant.LOGGER.info("saveToFile called");
+    }
+}
