9f0f54919966fc1bcbd7341f6e1fe6b0aa8ccba4
KostromDan <kostromdan@gmail.com>
2024-12-24 14:00:22 +0300

    Prevent simultaneous lang reading from 2 jvms.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/lang/LanguageProvider.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/lang/LanguageProvider.java
@@ -56,64 +56,64 @@ public class LanguageProvider {
 
     @SuppressWarnings("unchecked")
     public static void unzipAndUpdateLangFiles() {
-        LANG_PATH.toFile().mkdirs();
-        HashSet<String> langFilesInJarNames = new HashSet<>();
-        langFilesInJarNames.add("lang/en_us.json");
-        langFilesInJarNames.add("lang/ru_ru.json");
-        langFilesInJarNames.add("lang/zh_cn.json");
-        langFilesInJarNames.add("lang/README.md");
-        HashMap<String, HashMap<String, String>> jarLangFiles = new HashMap<>();
-        for (String langFile : langFilesInJarNames) {
-            if (langFile.endsWith("/")) {
-                continue;
-            }
-            String langFileName = langFile.split("/")[1];
-            if (!langFile.endsWith(".json")) {
-                JarInJarHelper.unzipFromJar(langFile, LANG_PATH.resolve(langFileName));
-                continue;
-            }
-            jarLangFiles.put(langFileName.split("\\.json")[0], JarInJarHelper.readJsonFromJar(langFile));
-        }
-
-        HashSet<Path> langFilesInConfigNames = getLangFilesInConfigPaths();
-        HashMap<String, HashMap<String, String>> configLangFiles = new HashMap<>();
-        for (Path path : langFilesInConfigNames) {
-            configLangFiles.put(path.getFileName().toString().split("\\.json")[0], JarInJarHelper.readJsonFromFile(path));
-        }
-
-        HashSet<String> allLanguages = new HashSet<>();
-        allLanguages.addAll(configLangFiles.keySet());
-        allLanguages.addAll(jarLangFiles.keySet());
-
-
-        Lang en_us_jar = new Lang(jarLangFiles.get("en_us"));
-        for (String langName : allLanguages) {
-            Lang jar = new Lang(jarLangFiles.getOrDefault(langName, new HashMap<>()));
-            final Lang unmodified_jar = new Lang((HashMap<String, String>) jar.lang.clone());
-            Lang config = new Lang(configLangFiles.getOrDefault(langName, new HashMap<>()));
-            config.lang.forEach((key, value) -> {
-                if (en_us_jar.lang.containsKey(key) && !Objects.equals(value, "$DEFAULT")) {
-                    jar.lang.put(key, value);
+        CrashAssistantConfig.executeWithLock(() -> {
+            LANG_PATH.toFile().mkdirs();
+            HashSet<String> langFilesInJarNames = new HashSet<>();
+            langFilesInJarNames.add("lang/en_us.json");
+            langFilesInJarNames.add("lang/ru_ru.json");
+            langFilesInJarNames.add("lang/zh_cn.json");
+            langFilesInJarNames.add("lang/README.md");
+            HashMap<String, HashMap<String, String>> jarLangFiles = new HashMap<>();
+            for (String langFile : langFilesInJarNames) {
+                if (langFile.endsWith("/")) {
+                    continue;
                 }
-            });
-            languages.put(langName, jar);
-            HashMap<String, String> outputJson = new HashMap<>();
+                String langFileName = langFile.split("/")[1];
+                if (!langFile.endsWith(".json")) {
+                    JarInJarHelper.unzipFromJar(langFile, LANG_PATH.resolve(langFileName));
+                    continue;
+                }
+                jarLangFiles.put(langFileName.split("\\.json")[0], JarInJarHelper.readJsonFromJar(langFile));
+            }
 
-            en_us_jar.lang.forEach((key, value) -> {
-                String outputValue;
-                if (Objects.equals(config.lang.get(key), "$DEFAULT")) {
-                    outputValue = "$DEFAULT";
-                } else if (Objects.equals(unmodified_jar.lang.get(key), config.lang.get(key))) {
-                    outputValue = "$DEFAULT";
-                } else if (!unmodified_jar.lang.containsKey(key) && Objects.equals(value, config.lang.get(key))) {
-                    outputValue = "$DEFAULT";
-                } else outputValue = config.lang.getOrDefault(key, "$DEFAULT");
-                outputJson.put(key, outputValue);
-            });
-            CrashAssistantConfig.executeWithLock(() -> {
+            HashSet<Path> langFilesInConfigNames = getLangFilesInConfigPaths();
+            HashMap<String, HashMap<String, String>> configLangFiles = new HashMap<>();
+            for (Path path : langFilesInConfigNames) {
+                configLangFiles.put(path.getFileName().toString().split("\\.json")[0], JarInJarHelper.readJsonFromFile(path));
+            }
+
+            HashSet<String> allLanguages = new HashSet<>();
+            allLanguages.addAll(configLangFiles.keySet());
+            allLanguages.addAll(jarLangFiles.keySet());
+
+
+            Lang en_us_jar = new Lang(jarLangFiles.get("en_us"));
+            for (String langName : allLanguages) {
+                Lang jar = new Lang(jarLangFiles.getOrDefault(langName, new HashMap<>()));
+                final Lang unmodified_jar = new Lang((HashMap<String, String>) jar.lang.clone());
+                Lang config = new Lang(configLangFiles.getOrDefault(langName, new HashMap<>()));
+                config.lang.forEach((key, value) -> {
+                    if (en_us_jar.lang.containsKey(key) && !Objects.equals(value, "$DEFAULT")) {
+                        jar.lang.put(key, value);
+                    }
+                });
+                languages.put(langName, jar);
+                HashMap<String, String> outputJson = new HashMap<>();
+
+                en_us_jar.lang.forEach((key, value) -> {
+                    String outputValue;
+                    if (Objects.equals(config.lang.get(key), "$DEFAULT")) {
+                        outputValue = "$DEFAULT";
+                    } else if (Objects.equals(unmodified_jar.lang.get(key), config.lang.get(key))) {
+                        outputValue = "$DEFAULT";
+                    } else if (!unmodified_jar.lang.containsKey(key) && Objects.equals(value, config.lang.get(key))) {
+                        outputValue = "$DEFAULT";
+                    } else outputValue = config.lang.getOrDefault(key, "$DEFAULT");
+                    outputJson.put(key, outputValue);
+                });
                 JarInJarHelper.writeJsonToFile(outputJson, LANG_PATH.resolve(langName + ".json"));
-            });
-        }
+            }
+        });
     }
 
     public static HashSet<Path> getLangFilesInConfigPaths() {
